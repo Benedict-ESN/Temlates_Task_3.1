384aba82e55ff6ad5907c621fc3cfd2207fc6138
Benedict.ESN <elder@elderscrolls.net>
2024-07-06 00:11:18 +0300

    first commit

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,33 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    protected Random random;
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        this.random = new Random();
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomIterator();
+    }
+
+private class RandomIterator implements Iterator<Integer> {
+    @Override
+    public boolean hasNext() {
+                return true;
+    }
+    @Override
+    public Integer next() {
+        // Генерируем случайное число в диапазоне [min, max]
+        return random.nextInt(max - min + 1) + min;
+    }
+}
+}
